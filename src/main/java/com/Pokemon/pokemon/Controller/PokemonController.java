23a504cd29736b3d9d66bd7020b78e707a0f4801
Sheylarp <rivera.sheyla.p@gmail.com>
2026-01-08 18:25:05 -0600

    perfil diseño css

--- src/main/java/com/Pokemon/pokemon/Controller/PokemonController.java
+++ src/main/java/com/Pokemon/pokemon/Controller/PokemonController.java
@@ -1,20 +1,37 @@
 package com.Pokemon.pokemon.Controller;
 
 
+import com.Pokemon.pokemon.Service.JwtService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("pokedex")
 public class PokemonController {
-
-    @GetMapping()
-    public String index() {
+    
+    @Autowired 
+    JwtService jwtUtil;
+    
+    @GetMapping
+    public String index(Model model, HttpSession session) {
+        String token = (String) session.getAttribute("token");
+        if (token != null) {
+            String userEmail = jwtUtil.extraerUsername(token);
+            model.addAttribute("userEmail", userEmail);
+        } else {
+            // Si no hay token, redirige al login
+            return "redirect:/login";
+        }
         return "Index";
     }
+
+    
     @GetMapping("detail/{email}")
     public String Form(@PathVariable("email") String email, Model model) {
         
