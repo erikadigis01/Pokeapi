abef6772a58b89625d65d1030ee5ac0609b277fc
Sheylarp <rivera.sheyla.p@gmail.com>
2026-01-09 13:15:39 -0600

    Actualizar usuario - solo ocupando controller

--- src/main/java/com/Pokemon/pokemon/Controller/PokemonController.java
+++ src/main/java/com/Pokemon/pokemon/Controller/PokemonController.java
@@ -1,6 +1,7 @@
 package com.Pokemon.pokemon.Controller;
 
 
+import com.Pokemon.pokemon.JPA.Roll;
 import com.Pokemon.pokemon.JPA.Usuario;
 import com.Pokemon.pokemon.Service.JwtService;
 import com.Pokemon.pokemon.Service.UsuarioService;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -41,15 +43,29 @@ public class PokemonController {
     public String Form(@PathVariable("email") String email, Model model) {
         
 //        //traer el id con el email
-//        Integer idUsuario = usuarioService.getUserIdByEmail(email);
+        Integer idUsuario = usuarioService.getUserIdByEmail(email);
 //        //traer el resto del usuario
-//        Usuario usuario = usuarioService.getById(idUsuario);
+        Usuario usuario = usuarioService.getById(idUsuario);
 //        //agregarlo al modelo
-//        model.addAttribute("usuario", usuario);
+        model.addAttribute("usuario", usuario);
         model.addAttribute("email", email);
         return "Perfil";
     }
     
+    @PostMapping("detail")
+    public String ActualizarDatos(@ModelAttribute("usuario") Usuario usuario) {
+        
+        Usuario user = usuarioService.getById(usuario.getId());
+        //sacar el roll y asignarlo
+        Roll roll = new Roll();
+        roll.setIdRoll(user.Roll.getIdRoll());
+        usuario.setRoll(roll);
+        //sacar el password y asignarlo
+        usuario.setPassword(user.getPassword());
+        //mandar al restcontroller 
+        usuarioService.update(user.getId(), usuario);
+        return "redirect:/pokedex/detail/" +  user.getEmail();
+    }
     
 
 }
